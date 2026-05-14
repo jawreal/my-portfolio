a1d4b15abe96e6d7514e318b93ede8aab007e7e0
Jorell Relleve <jorellrelleve@gmail.com>
2026-05-14 22:47:07 +0800

    feat: add get in touch form in contact

---
+++ src/components/GetInTouch.tsx
@@ -0,0 +1,50 @@
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { FaLinkedin } from 'react-icons/fa';
+import { SiGithub } from 'react-icons/si';
+
+const GetInTouch = () => {
+  return (
+  <form className="w-full flex flex-col [&_label]:font-bold [&_label]:text-sm [&_label]:dark:text-slate-200 gap-y-6"> 
+     <div className="flex flex-col gap-y-3">
+       <Label htmlFor="fullName">FULLNAME</Label>
+       <Input id="fullName" placeholder="your name (e.g., John Doe)" 
+         className="shadow-none border-x-0 border-t-0 border-b-2 rounded-none px-0 pb-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:border-b-2" />
+     </div>
+     <div className="flex flex-col gap-y-3">
+       <Label htmlFor="emailAddress">MESSAGE</Label>
+       <Input id="emailAddress" placeholder="your email (e.g., john.doe@mail.com)" className="shadow-none border-x-0 border-t-0 border-b-2 rounded-none px-0 pb-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:border-b-2" />
+     </div>
+     <div className="flex flex-col gap-y-3">
+       <Label htmlFor="message">EMAIL ADDRESS</Label>
+       <Textarea id="message" placeholder="your message (e.g., We're excited to explore how you can contribute to our team. Let's connect. )" rows={4} className="shadow-none border-x-0 border-t-0 border-b-2 rounded-none px-0 pb-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:border-b-2" />
+     </div>
+     <Button className="h-11 rounded-none mt-1">
+       SEND MESSAGE
+       <ArrowRight />
+     </Button>
+     <div className="flex flex-col gap-y-3">
+       <h5 className="font-extrabold dark:text-slate-200">MY EMAIL</h5>
+       <span className="text-slate-500 dark:text-slate-400">jorellrelleve@gmail.com</span>
+     </div>
+     <div className="flex flex-col gap-y-3">
+       <h5 className="font-extrabold dark:text-slate-200">SOCIAL PROFILES</h5>
+       <div className="w-full flex justify-start gap-x-2">
+         <Button variant="outline" className="h-11 rounded-none mt-1 rounded-none border-2">
+            <FaLinkedin />
+            LinkedIn
+         </Button>
+         <Button variant="outline" className="h-11 rounded-none mt-1 rounded-none border-2">
+            <SiGithub />
+            GitHub
+         </Button>
+       </div>
+     </div>
+  </form>
+  )
+}
+
+export default GetInTouch;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,12 +1,15 @@
+import GetInTouch from "@/components/GetInTouch";
+
 const Contact = () => {
   return (
-  <section className="w-full px-6 flex flex-col md:px-20 py-20 gap-y-6" id="contact">
+  <section className="w-full px-6 flex flex-col md:px-20 py-20 gap-y-10" id="contact">
      <div className="flex flex-col gap-y-2">
        <h1 className="text-4xl font-extrabold dark:text-slate-200">GET IN TOUCH</h1>
-       <p className="text-slate-500 dark:text-slate-400">Need a full-stack developer? Let's turn your idea into reality — hit me up for collaborations or projects.
+       <p className="text-slate-500 dark:text-slate-400 md:max-w-[24rem]">Need a full-stack developer? Let's turn your idea into reality — hit me up for collaborations or projects.
        </p>
      </div>
-     <div className="flex justify-center">
+     <div className="flex flex-col justify-center md:max-w-[25rem]">
+       <GetInTouch />
      </div>
   </section>
   )
