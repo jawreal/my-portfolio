8c205c51b002ece5067b69b0d25d7f3356921298
Jorell Relleve <jorellrelleve@gmail.com>
2026-05-14 23:03:35 +0800

    feat: change the design of stack badge and also its bg color

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -24,33 +24,33 @@ import {
 interface IStacks {
   icon: IconType;
   text: string;
-  bgColor: string;
+  color: string;
 }
 
 const stacks: Record<string, IStacks[]> = {
   frontend: [
-    { icon: SiHtml5, text: "HTML", bgColor: "#E34F26" },
-    { icon: FaCss3Alt, text: "CSS", bgColor: "#1572B6" },
-    { icon: SiJavascript, text: "JavaScript", bgColor: "#F7DF1E" },
-    { icon: SiTypescript, text: "TypeScript", bgColor: "#3178C6" },
-    { icon: SiTailwindcss, text: "Tailwind CSS", bgColor: "#06B6D4" },
-    { icon: SiReact, text: "React", bgColor: "#61DAFB" },
-    { icon: SiVite, text: "Vite", bgColor: "#646CFF" },
-    { icon: SiShadcnui, text: "shadcn/ui", bgColor: "#000000" },
-    { icon: SiReactquery, text: "TanStack", bgColor: "#FF4154" },
-    { icon: GiBearFace, text: "Zustand", bgColor: "#443E38" }
+    { icon: SiHtml5, text: "HTML", color: "text-[#E34F26]" },
+    { icon: FaCss3Alt, text: "CSS", color: "text-[#1572B6]" },
+    { icon: SiJavascript, text: "JavaScript", color: "text-[#F7DF1E]" },
+    { icon: SiTypescript, text: "TypeScript", color: "text-[#3178C6]" },
+    { icon: SiTailwindcss, text: "Tailwind CSS", color: "text-[#06B6D4]" },
+    { icon: SiReact, text: "React", color: "text-[#61DAFB]" },
+    { icon: SiVite, text: "Vite", color: "text-[#646CFF]" },
+    { icon: SiShadcnui, text: "shadcn/ui", color: "text-[#000000] dark:text-slate-200" },
+    { icon: SiReactquery, text: "TanStack", color: "text-[#FF4154]" },
+    { icon: GiBearFace, text: "Zustand", color: "text-[#443E38]" }
   ],
   backend: [
-    { icon: SiNodedotjs, text: "Node.js", bgColor: "#339933" },
-    { icon: SiExpress, text: "Express.js", bgColor: "#000000" },
-    { icon: SiSupabase, text: "Supabase", bgColor: "#3ECF8E" },
-    { icon: SiPostgresql, text: "PostgreSQL", bgColor: "#4169E1" },
-    { icon: SiMongodb, text: "MongoDB", bgColor: "#47A248" }
+    { icon: SiNodedotjs, text: "Node.js", color: "text-[#339933]" },
+    { icon: SiExpress, text: "Express.js", color: "text-[#000000] dark:text-slate-200" },
+    { icon: SiSupabase, text: "Supabase", color: "text-[#3ECF8E]" },
+    { icon: SiPostgresql, text: "PostgreSQL", color: "text-[#4169E1]" },
+    { icon: SiMongodb, text: "MongoDB", color: "text-[#47A248]" }
   ],
   tools: [
-    { icon: SiGit, text: "Git", bgColor: "#F05032" },
-    { icon: SiGithub, text: "GitHub", bgColor: "#181717" }, 
-    { icon: SiPostman, text: "Postman", bgColor: "#FF6C37" } 
+    { icon: SiGit, text: "Git", color: "text-[#F05032]" },
+    { icon: SiGithub, text: "GitHub", color: "text-[#000000] dark:text-slate-200" }, 
+    { icon: SiPostman, text: "Postman", color: "text-[#FF6C37]" } 
   ]
 } as const;
 
@@ -75,9 +75,9 @@ const Skills = () => {
             return (
             <Badge 
               key={stack.text} 
-              className="flex items-center gap-2 py-2 px-3 rounded-lg bg-white border border-slate-300 dark:border-slate-700 text-slate-950 shadow-none font-medium"
+              className="flex items-center gap-2 py-2 px-3 rounded-none bg-transparent border border-slate-300 dark:border-slate-700 shadow-none font-medium dark:text-slate-200 text-slate-950"
             >
-              <Icon size={18} color={stack.bgColor} />
+              <Icon size={18} className={stack.color} />
               <span>{stack.text}</span>
             </Badge>
             );
