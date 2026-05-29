ff42ed1df738e073c65852907589e4f7fe59b78b
Jorell Relleve <jorellrelleve@gmail.com>
2026-05-29 23:48:51 +0800

    feat: change toast color

--- src/components/CustomToast.tsx
+++ src/components/CustomToast.tsx
@@ -15,8 +15,8 @@ export const CustomToast = ({
   toast[status](description, {
     className: `${
       inLadingPage
-        ? "!bg-white !border-gray-200 !text-gray-500"
-        : "!border-gray-300 dark:!border-gray-800 font-inter !bg-gray-100 dark:!bg-gray-950 !text-gray-700 dark:!text-gray-200"
+        ? "!bg-white !border-slate-200 !text-slate-500"
+        : "!border-slate-300 dark:!border-slate-800 font-inter !bg-slate-100 dark:!bg-slate-950 !text-slate-700 dark:!text-slate-200"
     }`,
     duration: 3000,
     position: "bottom-right",
